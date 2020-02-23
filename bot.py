4e8ab4f3d9cfbf53f3e0d51bab3737f2a17b40ce
Mat Musko <muskomatt@outlook.com>
2020-02-23 15:12:53 +0100

    Zmieniono opis

--- bot.py
+++ bot.py
@@ -8,11 +8,13 @@ load_dotenv()
 
 bot = commands.Bot(command_prefix='^', description='First iteration of Apocryph')
 
+
 @bot.event
 async def on_ready():
     print("I'm ready!")
     print(bot.user.name)
     print(bot.user.id)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="students suffer"))
 
 
 @bot.command()
@@ -20,6 +22,7 @@ async def plan_zajec(ctx):
     plan = PlanScraperForApocryph()
     plan.get_plan()
     await ctx.send(file=discord.File(fp='Plans/PlanJPGS/plan.jpg'))
+
 
 
 bot.run(os.getenv('BOT_TOKEN'))
