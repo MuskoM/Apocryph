3e1b3333305c7939ad88e530d0c1d75f945e3b30
adamfoxman <adam.foxman@outlook.com>
2020-05-04 02:06:58 +0200

    Zaktualizowano wyświetlanie statystyk koronawirusa w Polsce

--- Coronavirus.py
+++ Coronavirus.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import discord
 from discord.ext import commands
 
+
 class Coronavirus(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -10,30 +11,40 @@ class Coronavirus(commands.Cog):
     @commands.command(name="koronawirus")
     async def covid(self, ctx, *args):
         """
-        Wyświetla aktualne zachorowania na koronawirusa
+        Wyświetla aktualne zachorowania na koronawirusa w Polsce
         """
-        site = requests.get(url="https://www.worldometers.info/coronavirus/")
+        site = requests.get(url="https://epidemia-koronawirus.pl")
         soup = BeautifulSoup(site.content, 'html.parser')
+        allCases = soup.find(id="panel-2-0-0-0").find(style="font-size: 48px;").text.strip().split()
+        curedCases = soup.find(id="panel-2-0-1-0").find(style="font-size: 48px;").text.strip().split()
+        deadCases = soup.find(id="panel-2-0-2-0").find(style="font-size: 48px;").text.strip().split()
         embedded = discord.Embed(
-            title="Coronavirus cases",
-            colour=discord.Colour.dark_red()
+            title="Coronavirus cases in Poland",
+            colour=discord.Colour.dark_red(),
         )
+        embedded.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/1024px-SARS-CoV-2_without_background.png")
+        embedded.set_footer(text="Source: epidemia-koronawirus.pl")
 
-        countries = soup.find('tbody').find_all('tr')
-        for i in countries:
-            if i.text.strip().startswith('Poland'):
-                lista = i.text.strip().split()
-                embedded.add_field(name='Kraj', value=lista[0])
-                embedded.add_field(name='Przypadki', value=lista[1])
-                if lista[2].startswith("+"):
-                    embedded.add_field(name='Nowe przypadki', value=lista[2])
+        embedded.add_field(name="All cases", value=allCases[0])
+        embedded.add_field(name="Cured cases", value=curedCases[0])
+        embedded.add_field(name="Dead cases", value=deadCases[0])
 
-        updateTime = soup.find_all(class_='container')[1].find_all(class_='row')[1].find(style="font-size:13px; color:#999; text-align:center").text
-        wynik = soup.find_all(class_='container')[1].find_all(class_='row')[1].find_all(id='maincounter-wrap')
+        await ctx.send(embed=embedded)
 
-        # embedded.add_field(name=updateTime)
-        # for i in wynik:
-        #     embedded.add_field(name=i.text)
+    @commands.command(name="powiaty")
+    async def covidPowiaty(self, ctx, *args):
+        """
+        Wyświetla link do mapy z przypadkami koronawirusa w poszczególnych powiatach.
+        :param ctx:
+        :param args:
+        :return:
+        """
+        embedded = discord.Embed(
+            title="Przypadki koronawirusa w powiatach",
+            colour=discord.Colour.dark_red(),
+        )
+        embedded.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/1024px-SARS-CoV-2_without_background.png")
+        embedded.add_field(name="Link do strony", value="https://bit.ly/2zSjHp3")
 
         await ctx.send(embed=embedded)
 
